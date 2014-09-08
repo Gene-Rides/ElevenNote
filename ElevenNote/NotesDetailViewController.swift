548f0d5a60644512db37fd1b2f1e05c850bf993e
ChaCha <ChaCha1@CC-AChoate-8.local>
2014-09-08 17:32:53 -0400

    connected more parts

--- ElevenNote/NotesDetailViewController.swift
+++ ElevenNote/NotesDetailViewController.swift
@@ -9,15 +9,41 @@
 import UIKit
 
 class NotesDetailViewController: UIViewController {
+   
+    var titleText: String = ""
+    
+    var completion : (    (String) -> () )?
+    var cancel : (() -> ())?
+    
+    @IBOutlet weak var titleField: UITextField!
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        titleLabel.text = titleText
+        titleField.text = titleText
     }
 
-    @IBOutlet weak var titleLabel: UILabel!
+//    @IBOutlet weak var titleLabel: UILabel!
     
-    var titleText: String = ""
+
     
+    @IBAction func SaveTapped(sender: AnyObject) {
+        if let doSave = completion {
+            doSave(titleField!.text!)
+        }
+        if self.navigationController != nil {
+            self.navigationController!.popViewControllerAnimated(true)
+        }
+    
+    }
+    @IBAction func CancelTapped(sender: AnyObject) {
+    
+        if let doCancel = cancel {
+            doCancel()
+        }
+        if self.navigationController != nil {
+            self.navigationController!.popViewControllerAnimated(true)
+        }
+    }
 }
 
