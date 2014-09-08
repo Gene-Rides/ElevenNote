360369dd3236d94dc142f77e5aa509f88486fad1
ChaCha <ChaCha1@CC-AChoate-8.local>
2014-09-08 15:32:23 -0400

    fixed the disaster

---
+++ ElevenNote/NotesDetailViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  ElevenNote
+//
+//  Created by ChaCha on 9/8/14.
+//  Copyright (c) 2014 ChaCha_gmo. All rights reserved.
+//
+
+import UIKit
+
+class NotesDetailViewController: UIViewController {
+    
+        
+
+}
+
--- ElevenNote/NotesViewController.swift
+++ ElevenNote/NotesViewController.swift
@@ -10,22 +10,18 @@ import UIKit
 
 class NotesViewController: UITableViewController {
     
-    var rows = ["Gene", "Dan", "George", "I don't know"]
+    var rows = ["Gene", "Joe", "Mark"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        //DO any Additiona
+        
     }
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
         return rows.count
     }
-
+    
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         
         var cell = UITableViewCell()
@@ -36,7 +32,5 @@ class NotesViewController: UITableViewController {
         
         return cell
     }
-    
-
 }
 
